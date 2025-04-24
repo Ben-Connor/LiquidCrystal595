725349a83df52c8126b9a977a5308d110fbc3262
Ben Connor <benconnor6472@gmail.com>
2025-04-24 02:24:02 +0100

    another

--- LiquidCrystal595.h
+++ LiquidCrystal595.h
@@ -131,7 +131,7 @@ private:
   uint8_t _datapin;
   uint8_t _latchpin;
   uint8_t _clockpin;
-  byte _register; //Stores the current state of the data
+  uint8_t _register; //Stores the current state of the data
  
   uint8_t _displayfunction;
   uint8_t _displaycontrol;
